1e72b638a96e3ccedb88a859c84f053821da34c8
Lukasz Biedak <Lukasz.Biedak@jeppesen.com>
2017-06-28 07:44:12 +0200

    Remove a lot of files

--- spring-mvc/src/main/java/hello/Application.java
+++ spring-mvc/src/main/java/hello/Application.java
@@ -2,9 +2,20 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
